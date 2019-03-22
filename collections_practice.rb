1b1d42469f77a68d5f404a1a40ef5dc0267119c6
Shawn Ren <doggy-ren@hotmail.com>
2019-03-22 18:17:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -75,8 +75,8 @@ def merge_data(array1, array2)
 end
 
 def merge_data(array1, array2)
-  new_hash_1 = array1[0].merge(array2[0].values[0])
-  new_hash_2 = array1[1].merge(array2[1].values[0])
+  new_hash_1 = array1[0].merge(array2[0].value[0])
+  new_hash_2 = array1[1].merge(array2[1].value[0])
   [new_hash_1, new_hash_2]
 end
 
